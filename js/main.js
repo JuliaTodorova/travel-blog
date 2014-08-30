64076802f928d04535e8cb77f3eefc36ee5f4946
Julia Todorova <juliastoycheva@gmail.com>
2014-08-30 10:19:15 -0700

    Added google maps

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ $(document).ready(function(){
 		$("#content").toggle();
 	});
 });
+// Google Map
+
+var map;
+
+function initialize(location)
+{
+	console.log(location);
+
+	var mapOptions = {
+		zoom: 2,
+		center: new google.maps.LatLng(0,0)
+	};
+
+	 map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+$(document).ready(function(){
+		navigator.geolocation.getCurrentPosition(initialize);
+});
