8fc25fe08a7ce0f702e01a1828e122069f9862a2
Julia Todorova <juliastoycheva@gmail.com>
2014-12-10 10:42:33 -0800

    City search filter

---
+++ js/backbone-city.js
@@ -0,0 +1,104 @@
+// Backbone
+
+
+// Routers
+
+app = {
+	models:{},
+	views:{},
+	collections:{},
+	routers:{},
+	init: function(){
+		directory = new app.views.Cities(directoryData);
+	}
+}
+
+// Models 
+
+app = app || {};
+
+app.models.City = Backbone.Model.extend({
+	defaults:{
+		'img': '',
+		'title': '',
+		'description': ''
+	}
+});
+
+// Collections
+
+app.collections.Cities = Backbone.Collection.extend({
+
+	model: app.models.City,
+
+	comparator: function(city){
+		return city.get('title');
+	}
+});
+
+
+// Views
+
+app = app ||{};
+
+app.views.City = Backbone.View.extend({
+	tagName: 'li',
+
+	template: _.template($('#city-template').html()),
+
+	render: function(){
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+
+
+app.views.Cities = Backbone.View.extend({
+	el:'#wrapper',
+
+	initialize: function(data) {
+		this.collection = new app.collections.Cities(data);
+		this.render();
+
+		this.on('change:searchFilter', this.filterBySearch, this);
+		this.collection.on('reset', this.render, this);
+	},
+
+	events: {
+		'keyup #searchBox': 'searchFilter'
+	},
+
+	render: function(){
+		var self = this;
+		$('#listing').empty();
+		_.each(this.collection.models, function(city){
+			self.renderCity(city);
+		}, this);
+	},
+
+	renderCity: function(city) {
+		var newcity = new app.views.City({
+			model: city
+		});
+		$('#listing').append(newcity.render().el);
+	},
+
+	searchFilter: function(e){
+		this.searchFilter = e.target.value;
+		this.trigger('change:searchFilter');
+	},
+
+	filterBySearch: function() {
+		this.collection.reset(directoryData, {silent: true});
+		var filterString = this.searchFilter,	
+		filtered = _.filter(this.collection.models, function(item){
+				return item.get('title').toLowerCase().indexOf(filterString.toLowerCase()) !== -1
+			});
+			this.collection.reset(filtered);
+	}
+});
+
+// app
+
+
+app.init();
