27fd0cdf6cf46b0d916db1cb59c48b5d8904804c
Julia Todorova <juliastoycheva@gmail.com>
2015-02-22 00:13:30 -0800

    Added checkbox continent filters

--- js/backbone-city.js
+++ js/backbone-city.js
@@ -1,6 +1,4 @@
-// Backbone
-
-
+// City Filter
 // Routers
 
 app = {
@@ -21,31 +19,28 @@ app.models.City = Backbone.Model.extend({
 	defaults:{
 		'img': '',
 		'title': '',
-		'description': ''
+		'description': '',
+		'continent': '',
+		'url': ''
 	}
 });
 
 // Collections
 
 app.collections.Cities = Backbone.Collection.extend({
-
 	model: app.models.City,
-
 	comparator: function(city){
 		return city.get('title');
 	}
 });
-
 
 // Views
 
 app = app ||{};
 
 app.views.City = Backbone.View.extend({
-	tagName: 'li',
-
+	tagName: 'li > a',
 	template: _.template($('#city-template').html()),
-
 	render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
@@ -55,17 +50,16 @@ app.views.City = Backbone.View.extend({
 
 app.views.Cities = Backbone.View.extend({
 	el:'#wrapper',
-
 	initialize: function(data) {
 		this.collection = new app.collections.Cities(data);
 		this.render();
-
 		this.on('change:searchFilter', this.filterBySearch, this);
 		this.collection.on('reset', this.render, this);
 	},
 
 	events: {
-		'keyup #searchBox': 'searchFilter'
+		'keyup #searchBox': 'searchFilter',
+		'click #checkBox input:checkbox': 'filterBySearch'
 	},
 
 	render: function(){
@@ -83,18 +77,30 @@ app.views.Cities = Backbone.View.extend({
 		$('#listing').append(newcity.render().el);
 	},
 
+	searchContinents: function(){
+		return $("#checkBox input:checkbox:checked").toArray().map(function(el){
+			return $(el).val();
+		});	
+	},
+
+	searchString: function(){
+		return $('#searchBox').val();
+	},
+
 	searchFilter: function(e){
-		this.searchFilter = e.target.value;
+		this.searchText = e.target.value;
 		this.trigger('change:searchFilter');
 	},
 
 	filterBySearch: function() {
 		this.collection.reset(directoryData, {silent: true});
-		var filterString = this.searchFilter,	
+		var filterString = this.searchText || this.searchString();
+		var continents = this.searchContinents();
 		filtered = _.filter(this.collection.models, function(item){
-				return item.get('title').toLowerCase().indexOf(filterString.toLowerCase()) !== -1
-			});
-			this.collection.reset(filtered);
+			return (item.get('title').toLowerCase().indexOf(filterString.toLowerCase()) !== -1) &&
+				(_.isEmpty(continents) || _.indexOf(continents, item.get('continent')) !== -1)
+		});
+		this.collection.reset(filtered);
 	}
 });
 
